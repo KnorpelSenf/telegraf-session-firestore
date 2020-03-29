ef25326f9db18983d9a6d3539488d3fac82ab9bf
Steffen <steffen@indigo-nms.eu>
2020-03-29 15:29:45 +0200

    Support firestore

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # telegraf-session-firestore
+
 Google Firestore based session middleware for telegraf.js. Heavily inspired by https://github.com/telegraf/telegraf-session-firebase/
+
+## Installation
+
+```bash
+npm install --save telegraf-session-firestore
+```
+
+## Example
+
+```js
+const Telegraf = require('telegraf')
+const firestoreSession = require('telegraf-session-firestore')
+const { Firestore } = require('@google-cloud/firestore')
+
+const db = new Firestore({
+    projectId: 'YOUR_PROJECT_ID',
+    keyFilename: 'firestore-keyfile.json',
+});
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(firestoreSession(db.collection('sessions')))
+bot.on('text', (ctx, next) => {
+  ctx.session.counter = ctx.session.counter || 0
+  ctx.session.counter++
+  return next()
+})
+bot.hears('/stats', ({ reply, session, from }) => reply(`${session.counter} messages from ${from.username}`))
+bot.startPolling()```
---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = (collection, opts) => {
+    const options = Object.assign({
+        property: 'session',
+        getSessionKey: (ctx) => ctx.from && ctx.chat && `${ctx.from.id}-${ctx.chat.id}`
+    }, opts)
+
+    function getSession(key) {
+        return collection.doc(key).get()
+            .then((snapshot) => {
+                if (!snapshot.exists) return undefined
+                return snapshot.data()
+            })
+    }
+
+    function saveSession(key, session) {
+        if (!session || Object.keys(session).length === 0) {
+            return collection.doc(key).delete()
+        }
+        return collection.doc(key).set(session)
+    }
+
+    return (ctx, next) => {
+        const key = options.getSessionKey(ctx)
+        if (!key) {
+            return next()
+        }
+        return getSession(key).then((value) => {
+            let session = value || {}
+            Object.defineProperty(ctx, options.property, {
+                get: function () { return session },
+                set: function (newValue) { session = Object.assign({}, newValue) }
+            })
+            return next().then(() => saveSession(key, session))
+        })
+    }
+}
